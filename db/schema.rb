5afd18309c8a443bdad05b74d8ace020c52b0ad6
Beth Knight <bethknight1234@gmail.com>
2017-05-02 11:56:02 -0600

    item request index show actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502013106) do
+ActiveRecord::Schema.define(version: 20170502172444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20170502013106) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "unit_price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "merchants", force: :cascade do |t|
---
+++ spec/support/factories/items.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :item do
+    name "Boxer"
+    description "The best dog breed ever"
+    unit_price 1
+    created_at "2017-05-02 11:24:44"
+    updated_at "2017-05-02 11:24:44"
+  end
+end
