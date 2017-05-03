d9ea1f59173e3027556649fdaf8fdefab96058a8
bschwartz10 <bschwartz92@gmail.com>
2017-05-03 11:33:30 -0600

    merchant and item relationship working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502213836) do
+ActiveRecord::Schema.define(version: 20170503170709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20170502213836) do
     t.integer  "unit_price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "merchant_id"
+    t.index ["merchant_id"], name: "index_items_on_merchant_id", using: :btree
   end
 
   create_table "merchants", force: :cascade do |t|
@@ -57,4 +59,5 @@ ActiveRecord::Schema.define(version: 20170502213836) do
     t.datetime "updated_at"
   end
 
+  add_foreign_key "items", "merchants"
 end
--- lib/tasks/import_data.rake
+++ lib/tasks/import_data.rake
@@ -51,10 +51,11 @@ namespace :import_data do
   desc "import items data"
   task :import_items_csv => :environment do
     CSV.foreach("db/csv/items.csv", headers: true, header_converters: :symbol) do |row|
-      Item.create(
+      Item.create!(
         name: row[:name],
         description: row[:description],
         unit_price: row[:unit_price],
+        merchant_id: row[:merchant_id],
         created_at: row[:created_at],
         updated_at: row[:updated_at]
       )
