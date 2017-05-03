eaa46e7c4461a653b80ae2cf981e9be500a94f58
bschwartz10 <bschwartz92@gmail.com>
2017-05-03 13:10:24 -0600

    add migration to transaction

--- spec/support/factories/invoice_items.rb
+++ spec/support/factories/invoice_items.rb
@@ -2,6 +2,8 @@ FactoryGirl.define do
   factory :invoice_item do
     quantity 1
     unit_price 12345
+    invoice
+    item
     created_at "2017-05-02 13:17:46"
     updated_at "2017-05-02 13:17:46"
   end
