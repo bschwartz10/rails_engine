553e4acd595000546d78fe7d92930ff51073c675
bschwartz10 <bschwartz92@gmail.com>
2017-05-03 21:55:52 -0600

    first 2 single merchant queries written

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,14 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
   has_many :customers, through: :invoices
+  has_many :invoice_items, through: :invoices
+
+  def revenue
+    invoices.join(invoice_items: :transactions).merge(Transaction.successful).sum("quantity * unit_price")
+  end
+
+  def revenue_by_date(date)
+    invoices.where("created_at" => date).joins(:invoice_items).sum("quantity * unit_price")
+  end
+
 end
